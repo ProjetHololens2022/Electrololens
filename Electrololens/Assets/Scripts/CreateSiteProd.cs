f0ddfb19d8fd7fceb014b46f44d82475b54090d9
Savro01 <59883445+Savro01@users.noreply.github.com>
2022-04-26 10:34:44 +0200

    Us finit
    On peut faire spawn des objets now

--- Electrololens/Assets/Scripts/CreateSiteProd.cs
+++ Electrololens/Assets/Scripts/CreateSiteProd.cs
@@ -10,12 +10,16 @@ public class CreateSiteProd : MonoBehaviour
 
     public void CreateNewSiteProd()
     {
+        GameObject plateforme = GameObject.FindGameObjectsWithTag("Plateforme")[0].transform.GetChild(1).gameObject;
         camera = Camera.main;
         Vector3 camVect;
         Vector3 camForward = Camera.current.transform.forward;
         camVect = Camera.current.transform.position + new Vector3(Vector3.Dot(camForward, new Vector3(1f, 0f, 0f)), 0f, Vector3.Dot(camForward, new Vector3(0f, 0f, 1f))) * 1.2f;
         GameObject go = (GameObject)Instantiate(objet, camVect, Quaternion.identity);
-        //GameObject.FindGameObjectsWithTag("Virtual Scene")[0].GetComponent<VirtualObjectScene>().AddVirtualChild(go.gameObject);
+        go.transform.parent = plateforme.transform;
+        go.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 scale = plateforme.transform.GetChild(0).localScale;
+        go.transform.localScale = scale;
         //Positionne le nouveau site au centre de la zone de jeu
     }
 }
