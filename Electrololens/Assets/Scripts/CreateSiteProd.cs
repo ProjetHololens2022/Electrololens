2ee3ea68a9b9c58c4da2b6470de1b98d3ff5e8da
Savro01 <59883445+Savro01@users.noreply.github.com>
2022-05-03 10:00:15 +0200

    Boite a lego fonctionnelle

--- Electrololens/Assets/Scripts/CreateSiteProd.cs
+++ Electrololens/Assets/Scripts/CreateSiteProd.cs
@@ -6,17 +6,25 @@ public class CreateSiteProd : MonoBehaviour
 {
     [SerializeField]
     private GameObject prefabPanier;
+    [SerializeField]
     private GameObject prefabPlatform;
     private Vector3 coordBase = new Vector3(0.0f, 0.0f, 0.0f);
     private GameObject platform;
     //private Transform ellipse = null;
-    Camera camera;
 
     public void Start()
     {
         coordBase = prefabPanier.transform.localPosition;
         platform = GameObject.FindGameObjectsWithTag("Plateforme")[0].transform.GetChild(1).gameObject;
-        prefabPlatform = GameObject.FindGameObjectsWithTag("CentraleNucleaire")[0];
+    }
+
+    public void holdPrefabPanier()
+    {
+        GameObject go = (GameObject)Instantiate(prefabPanier, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        go.transform.parent = prefabPanier.transform.parent;
+        go.transform.localPosition = coordBase;
+        Vector3 scale = platform.transform.GetChild(0).localScale;
+        go.transform.localScale = scale;
     }
 
     public void UnholdPrefabPanier()
@@ -26,44 +34,23 @@ public class CreateSiteProd : MonoBehaviour
         //Si le prefab est dans la gamezone.platform
         if (isInsinePlatform(prefabpanierX, prefabpanierZ))
         {
-/*            Vector3 camVect;
-            Debug.Log(Camera.current);
-            Vector3 camForward = Camera.main.transform.forward;
-            camVect = Camera.main.transform.position + new Vector3(Vector3.Dot(camForward, new Vector3(1f, 0f, 0f)), 0f, Vector3.Dot(camForward, new Vector3(0f, 0f, 1f))) * 1.2f;*/
             GameObject go = (GameObject)Instantiate(prefabPlatform, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
             go.transform.parent = platform.transform;
-            go.transform.localPosition = new Vector3(prefabpanierX*50.0f, 0.0f, prefabpanierX*50.0f);
+            go.transform.localPosition = new Vector3((prefabpanierX-platform.transform.position.x)*50.0f, 0.0f, (prefabpanierZ-platform.transform.position.z)*50.0f);
             Vector3 scale = platform.transform.GetChild(0).localScale;
             go.transform.localScale = scale;
         }
-        prefabPanier.transform.localPosition = coordBase;
+        Destroy(prefabPanier);
     }
 
     public bool isInsinePlatform(float X, float Z)
     {
-        
-        if (platform.transform.position.x - 0.3f < X & X < platform.transform.position.x + 0.3f & platform.transform.position.z - 0.2f < Z & Z < platform.transform.position.z + 0.2f)
+        float platformX = platform.transform.position.x;
+        float platformZ = platform.transform.position.z;
+        if (platformX - 0.3f < X & X < platformX + 0.3f & platformZ - 0.2f < Z & Z < platformZ + 0.2f)
         {
             return true;
         }
         return false;
-        /*if (ellipse == null)
-        {
-            return;
-        }
-        const float long_diameter = 0.28f;
-        const float small_diameter = 0.18f;
-        Vector3 localDir = transform.Position - ellipse.position; //Angle 0
-        localDir = Quaternion.Euler(0, ellipse.eulerAngles.y, 0) * localDir; //Angle X
-        float equation = Mathf.Pow(localDir.x, 2.0f) / Mathf.Pow(long_diameter, 2.0f) + Mathf.Pow(localDir.z, 2.0f) / Mathf.Pow(small_diameter, 2.0f);
-        if (equation > 1.0f)
-        {
-            localDir = Vector3.Normalize(localDir);
-            localDir = Quaternion.Euler(0, -2.0f * ellipse.eulerAngles.y, 0) * localDir; //Angle -X
-            localDir.x *= long_diameter;
-            localDir.z *= small_diameter;
-            localDir = Quaternion.Euler(0, ellipse.eulerAngles.y, 0) * localDir; //Angle 0
-            transform.Position = ellipse.position + localDir;
-        }*/
     }
 }
