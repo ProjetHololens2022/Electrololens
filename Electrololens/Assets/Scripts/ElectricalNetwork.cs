5a76d000e3dbda4bfba04718949a1b687bf3ea11
Léo <58537513+leolaf@users.noreply.github.com>
2022-05-04 02:35:51 +0200

    LES CONNEXIONS OMG

--- Electrololens/Assets/Scripts/ElectricalNetwork.cs
+++ Electrololens/Assets/Scripts/ElectricalNetwork.cs
@@ -7,19 +7,28 @@ public class ElectricalNetwork : MonoBehaviour
     List<GameObject> producers = new List<GameObject>();
     List<GameObject> consumers = new List<GameObject>();
 
+    [SerializeField]
+    private GameObject linePrefab;
+
     public void addBuilding(GameObject go){
         if(go.GetComponent<ProducteurClass>() != null){
             if(!producers.Contains(go)){
                 producers.Add(go);
                 print("ajout d'un producteur.");
-                drawLine(go);
+                GameObject line = Instantiate(linePrefab, new Vector3(0.0f,0.0f,0.0f), Quaternion.identity);
+                line.transform.parent = this.transform;
+                line.GetComponent<LineConnector>().SetStart(go.transform.Find("Sphere"));
+                line.GetComponent<LineConnector>().SetEnd(this.transform.Find("Sphere"));
             }
         }
         if(go.GetComponent<ConsommateurClass>() != null){
             if(!consumers.Contains(go)){
                 consumers.Add(go);
                 print("ajout d'un consommateur.");
-                drawLine(go);
+                GameObject line = Instantiate(linePrefab, new Vector3(0.0f,0.0f,0.0f), Quaternion.identity);
+                line.transform.parent = this.transform;
+                line.GetComponent<LineConnector>().SetStart(go.transform.Find("Sphere"));
+                line.GetComponent<LineConnector>().SetEnd(this.transform.Find("Sphere"));
             }
         }
     }
@@ -34,29 +43,6 @@ public class ElectricalNetwork : MonoBehaviour
 
     public double getConsumption(){
         return getProduction()/(double)consumers.Count;
-    }
-
-    private void drawLine(GameObject go){
-        GameObject lineGo = new GameObject();
-        lineGo.AddComponent<LineRenderer>();
-        LineRenderer line = lineGo.GetComponent<LineRenderer>();
-        line.positionCount = 100;
-        line.widthMultiplier = 0.002f;
-        Vector3 pos1 = go.transform.Find("Sphere").position;
-        Vector3 pos3 = this.transform.Find("Sphere").position;
-        Vector3 pos2 = pos1 + (pos3-pos1)/2.0f;
-        pos2.y -= 0.02f;
-        Vector3[] positions = new Vector3[100];
-        for(int i = 0; i < 100; ++i){
-            float t = ((float) i) / 99.0f;
-            Vector3 lerp12 = Vector3.Lerp(pos1,pos2,t);
-            Vector3 lerp23 = Vector3.Lerp(pos2,pos3,t);
-            positions[i] = Vector3.Lerp(lerp12,lerp23,t);
-        }
-        line.SetPositions(positions);
-        line.alignment = LineAlignment.View;
-        line.loop = true;
-        lineGo.transform.parent = this.transform;
     }
 
 }
