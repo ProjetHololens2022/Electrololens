6806ea15ea7fd00037cb64e8b79c7b5b9cd7d0fe
Savro01 <59883445+Savro01@users.noreply.github.com>
2022-05-09 15:32:42 +0200

    Ajout json stat emissionCO2

--- Electrololens/Assets/Scripts/WrenchHitter.cs
+++ Electrololens/Assets/Scripts/WrenchHitter.cs
@@ -6,8 +6,6 @@ public class WrenchHitter : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Hello !");
-        Debug.Log(other);
         if(other.gameObject.GetComponent<ProducteurClass>() != null)
         {
             other.gameObject.GetComponent<ProducteurClass>().reparationEtat();
